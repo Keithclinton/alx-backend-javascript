53f7349ff23e9f22b5736c52a08b800108ad0b47
Keith Clinton <keithnyaburi@gmail.com>
2024-07-26 12:28:03 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -5,3 +5,4 @@ export default function getResponseFromAPI() {
     // if it's a success call the resolve function and pass the data
     // if there's an error call reject and pass the error through
   });
+}
